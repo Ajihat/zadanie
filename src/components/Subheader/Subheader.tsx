99b905c8a33463adc7ea9332b433704e183536f7
Dawid Lechowski <david.lechowski@gmail.com>
2023-06-15 15:17:53 +0200

    add media query rules for mobile devices

--- src/components/Subheader/Subheader.tsx
+++ src/components/Subheader/Subheader.tsx
@@ -7,7 +7,7 @@ export const Subheader = ({ text }: SubheaderProps) => {
 		<Typography
 			variant='h6'
 			component='h2'
-			sx={{ padding: '10px', backgroundColor: 'rgb(240,240,240)', fontSize: 'px' }}
+			sx={{ padding: '10px', backgroundColor: 'rgb(220,220,220)', fontSize: 'px' }}
 		>
 			{text}
 		</Typography>
--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -4,11 +4,12 @@ export const container: SxProps = {
 	display: 'flex',
 	justifyContent: 'space-between',
 	alignItems: 'flex-start',
+	flexWrap: 'wrap',
 	padding: '0 24px',
 };
 
 export const inner: SxProps = {
 	position: 'relative',
-	width: '48%',
+	width: { xs: '100%', lg: '48%' },
 	minHeight: '50vh',
 };
