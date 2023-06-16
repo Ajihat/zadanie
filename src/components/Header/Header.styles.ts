5459698a33bdebcd6b527166b8869e4e62a9d3c1
Dawid Lechowski <david.lechowski@gmail.com>
2023-06-16 10:12:23 +0200

    add Header styles file

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,9 @@
+import { SxProps } from '@mui/material';
+
+export const icon: SxProps = {
+	fontSize: '30px',
+};
+
+export const iconHolder: SxProps = {
+	mr: 4,
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import { Box, AppBar, Toolbar, Icon, Typography } from '@mui/material';
 import { Forum } from '@mui/icons-material';
 
+import * as styles from './Header.styles';
+
 export const Header = () => {
 	return (
 		<Box>
 			<AppBar position='static'>
 				<Toolbar>
-					<Icon sx={{ mr: 4 }}>
-						<Forum sx={{ fontSize: '30px' }} />
+					<Icon sx={styles.iconHolder}>
+						<Forum sx={styles.icon} />
 					</Icon>
 					<Typography variant='h5' component='h1'>
 						POSTS FINDER
